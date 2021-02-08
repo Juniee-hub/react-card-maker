87664577cca34b59724bc613a9f28102822e14fb
junes3754 <junes3754@gmail.com>
2021-02-08 23:20:45 +0900

    Login 구현

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React        from 'react';
+import ReactDOM     from 'react-dom';
 import './index.module.css';
-import App from './app';
+import App          from './app';
+import Auth_service from "./service/auth_service";
 
+const authService = new Auth_service()
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App authService={authService}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
