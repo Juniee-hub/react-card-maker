e73408a258afeeedef40d970f6bed4d114a43b9b
junes3754 <junes3754@gmail.com>
2021-02-12 21:16:43 +0900

    Login 구성

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,11 @@ import React  from 'react';
 import styles from './footer.module.css';
 
 const Footer = (props) => {
-    return (<h1>Footer</h1>);
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.title}>Code Your Dream</p>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = (props) => {
-    return (<h1>Header</h1>);
+const Header = ({onLogout}) => {
+    return (
+        <header className={styles.header}>
+            {onLogout && (<button className={styles.logout} onClick={onLogout}>로그아웃</button>)}
+            <img className={styles.logo} src="/images/logo.png" alt="logo"/>
+            <h1 className={styles.title}>명함관리</h1>
+    </header>
+    );
 };
 
 export default Header;
