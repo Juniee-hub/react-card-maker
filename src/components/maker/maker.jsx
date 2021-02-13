276be7002bff03eee163165533e526f32f425fa2
junes3754 <junes3754@gmail.com>
2021-02-13 23:04:22 +0900

    CSS 스타일링

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,12 +1,49 @@
-import React,{ useEffect } from 'react';
+import React,{ useEffect,useState } from 'react';
 import styles              from './maker.module.css';
 import Header              from "../header/header";
 import Footer              from "../footer/footer";
 import { useHistory }      from "react-router-dom";
+import Editor              from "../editor/editor";
+import Preview             from "../preview/preview";
 
 
 const Maker = ({authService}) => {
 
+    const [cards,setCards] = useState([
+        {
+            id:'1',
+            name:'June',
+            company:'Plani',
+            theme:'light',
+            title:'Engineer',
+            email:'test@gmail.com',
+            message:'테스트!',
+            fileName:'테스트..',
+            fileURL:'테스트...png',
+        },
+        {
+            id:'2',
+            name:'June2',
+            company:'Plani',
+            theme:'colorful',
+            title:'Engineer',
+            email:'test@gmail.com',
+            message:'테스트!',
+            fileName:'테스트..',
+            fileURL:null,
+        },
+        {
+            id:'3',
+            name:'June3',
+            company:'Plani',
+            theme:'dark',
+            title:'Engineer',
+            email:'test@gmail.com',
+            message:'테스트!',
+            fileName:'테스트..',
+            fileURL:null,
+        }
+    ])
     const history = useHistory();
 
     const onLogout = ()=>{
@@ -24,6 +61,11 @@ const Maker = ({authService}) => {
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
+            <div className={styles.container}>
+                <Editor cards={cards} />
+                <Preview cards={cards} />
+
+            </div>
             <Footer />
         </section>
     );
